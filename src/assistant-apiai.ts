67b399e9c2aa363838c3fc69eeb643e237ee4719
Antonius Ostermann <ostermann.antonius@gmail.com>
2017-06-26 14:04:17 +0200

    fixes package.json

--- src/assistant-apiai.ts
+++ src/assistant-apiai.ts
@@ -1,3 +1,5 @@
 export * from "./components/apiai/interfaces";
 export { descriptor } from "./components/apiai/descriptor";
 export { SpecHelper } from "./spec-helper";
+export { ApiAiHandle } from "./components/apiai/handle";
+export { Extractor } from "./components/apiai/extractor";
