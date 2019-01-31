ae3872e47cbbe12f6855094e8ee1fe503285d665
Thorsten Woywod <t.woywod@web.de>
2019-01-31 14:22:48 +0100

    exports dialogflowEvents

--- src/assistant-apiai.ts
+++ src/assistant-apiai.ts
@@ -1,6 +1,6 @@
 export * from "./components/apiai/public-interfaces";
+export * from "./components/apiai/dialogflow-events";
 export { descriptor } from "./components/apiai/descriptor";
 export { ApiAiSpecHelper as SpecHelper } from "./spec-helper";
 export { ApiAiHandler } from "./components/apiai/handler";
 export { Extractor } from "./components/apiai/extractor";
-
