7454d75a9e713dfcb1f26e8a48c3c4769d6bad90
Thorsten Woywod <t.woywod@web.de>
2019-02-15 14:19:25 +0100

    adds interface definitions

--- src/components/apiai/private-interfaces.ts
+++ src/components/apiai/private-interfaces.ts
@@ -9,6 +9,9 @@ export namespace Configuration {
 
     /** If set to true and if there is no chat bubble text, "displayText" will be set to the voiceResponse, default: true */
     defaultDisplayIsVoice: boolean;
+
+    /** Path to the Google Assistant service account access key. Take a look at @Link {https://dialogflow.com/docs/reference/v2-auth-setup} */
+    googleApplicationCredentials: string;
   }
 
   /** Required configuration options, no defaults are used here */
@@ -18,7 +21,7 @@ export namespace Configuration {
      * assistant-apiai checks if all headers are present and contain the respective value.
      * To configure, go to the "fulfillment" tab in your dialogflow console and add some secret header keys and (complex) values.
      * After that, add them to this object, for example: {"myFirstSecretHeader": "myVerySecretValue", "mySecondSecretHeader": "mySecondVerySecretValue"}.
-     * That way, you are able to verify that an incomming request was really sent by your dialogflow agent.
+     * That way, you are able to verify that an incoming request was really sent by your dialogflow agent.
      */
     authenticationHeaders: { [name: string]: string };
   }
@@ -29,3 +32,20 @@ export namespace Configuration {
 
 /** Name of component */
 export const COMPONENT_NAME = "apiai";
+
+/**
+ * The response of the dialogflow agent export api.
+ */
+export interface DialogflowExportResponse {
+  name: string;
+  done: boolean;
+  response: { agentContent: string };
+}
+
+/**
+ * Identifiers for all local bound services
+ */
+export const localServices = {
+  /** Inject an instance of the @type{DialogflowClient}  */
+  dialogflowClient: Symbol("dialogflow-client"),
+};
