dc24a7c70d4ca028ed2ac6a41f1e14467a6bece6
Thorsten Woywod <t.woywod@web.de>
2019-03-27 14:29:55 +0100

    improve deployment specs

--- spec/src/components/apiai/deployment.spec.ts
+++ spec/src/components/apiai/deployment.spec.ts
@@ -6,24 +6,29 @@ import { COMPONENT_NAME, Configuration } from "../../../../src/components/apiai/
 import { ThisContext } from "../../../support/this-context";
 
 interface CurrentThisContext extends ThisContext {
+  /** Instance of the current ApiAiDeployment */
   deployment: ApiAiDeployment;
+  /** Current build directory */
   buildDir: string;
+  /** Instance of the current DialogflowClient */
   dialogflowClient: DialogflowClient;
-  spys: any;
+  /** Placeholder for all used spy objects */
+  spies: any;
+  /** Current build timestamp */
   buildTimeStamp: number;
 }
 
 describe("ApiAiDeployment", function() {
   beforeEach(async function(this: CurrentThisContext) {
-    this.spys = {};
-    this.spys.exportConfig = jasmine.createSpy("exportConfig");
-    this.spys.restoreConfig = jasmine.createSpy("restoreConfig");
+    this.spies = {};
+    this.spies.exportConfig = jasmine.createSpy("exportConfig");
+    this.spies.restoreConfig = jasmine.createSpy("restoreConfig");
 
     this.buildTimeStamp = Date.now();
 
     this.dialogflowClient = {
-      restoreConfig: this.spys.restoreConfig,
-      exportConfig: this.spys.exportConfig,
+      restoreConfig: this.spies.restoreConfig,
+      exportConfig: this.spies.exportConfig,
     } as any;
 
     /** Create an instance of the ApiAiDeployment */
@@ -38,11 +43,11 @@ describe("ApiAiDeployment", function() {
     });
 
     it("will export the given configuration as an export", async function(this: CurrentThisContext) {
-      expect(this.spys.exportConfig).toHaveBeenCalledWith(this.buildDir);
+      expect(this.spies.exportConfig).toHaveBeenCalledWith(this.buildDir);
     });
 
     it("will restore the generated configuration to the given dialogflow agent", async function(this: CurrentThisContext) {
-      expect(this.spys.restoreConfig).toHaveBeenCalledWith(this.buildDir);
+      expect(this.spies.restoreConfig).toHaveBeenCalledWith(this.buildDir);
     });
   });
 });
